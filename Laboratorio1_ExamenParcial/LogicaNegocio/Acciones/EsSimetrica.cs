0e38e752bfbe0da110b22d918bcb3444f10430bc
betomorales <betomorales@outlook.com>
2016-03-16 10:10:01 -0600

    Añadiendo Simétrica.

---
+++ Laboratorio1_ExamenParcial/LogicaNegocio/Acciones/EsSimetrica.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio1_ExamenParcial.LogicaNegocio.Acciones
+{
+    public class EsSimetrica
+    {
+
+        public bool Calcular(double[,] matrizUno, int maxCol, int maxFila)
+        {
+            /* TO BE MOVED TO VALIDACIONES */
+            if (maxCol != maxFila) { return false; }
+
+            double[,] matrizTranspuesta = new Double[maxCol, maxFila];
+
+            Transpuesta resul = new Transpuesta();
+            matrizTranspuesta = resul.Calcular(matrizUno, maxCol, maxFila);
+
+            int i = 0;
+            int j = 0;
+
+            bool sonIguales = true;
+
+            for(i = 0; i < maxCol; i++)
+            {
+                for(j = 0; j < maxFila; j++)
+                {
+                    if(matrizUno[i,j] != matrizTranspuesta[i, j])
+                    {
+                        sonIguales = false;
+                    }
+
+                }
+
+            }
+
+            return sonIguales;
+        }
+        }
+}
---
+++ PruebaCopiar/5-EsSimetrica.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebaCopiar
+{
+    [TestClass]
+    public class _5_EsSimetrica
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            double[,] laMatrizUno = new double[3, 3];
+            laMatrizUno[0, 0] = 2;
+            laMatrizUno[0, 1] = 1;
+            laMatrizUno[0, 2] = 3;
+            laMatrizUno[1, 0] = 1;
+            laMatrizUno[1, 1] = 0;
+            laMatrizUno[1, 2] = -2;
+            laMatrizUno[2, 0] = 3;
+            laMatrizUno[2, 1] = -2;
+            laMatrizUno[2, 2] = -47;
+
+            bool resultadoEsperado = true;
+
+            bool resultado;
+
+            Laboratorio1_ExamenParcial.LogicaNegocio.Acciones.EsSimetrica resul = new Laboratorio1_ExamenParcial.LogicaNegocio.Acciones.EsSimetrica();
+            resultado = resul.Calcular(laMatrizUno, 3, 3);
+
+            Assert.AreEqual(resultado, resultadoEsperado);
+
+        }
+    }
+}
